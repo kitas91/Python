c9a3efbc253bc419604acc2dbd198379eb260c44
Kitas91 <gowland.lucas@gmail.com>
2020-01-06 22:28:43 -0500

    final clean sheet

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,69 +1,61 @@
-#import proper libraries
+# Import proper libraries
 import numpy as np
 import pandas as pd
 
-#create csv paths and import data into vsc
+# Create csv paths and import data into vsc
 data = pd.read_csv('PyBank_data.csv', index_col="Date")
-#set column as "Profit/Losses"
+# Set column as "Profit/Losses"
 columns = ["Profit/Losses"]
 data.columns = columns
 
-#test data, make sure you are importing using .head or .tail
-data.tail(10)
-
-#calculating the daily percent change - How do I commit this command to add the column permanently on my dataframe? 
-percent_change = data.pct_change()
-percent_change.tail()
-
-#adding % change to the table as the third entry
-data["% chg"] = percent_change
+# Test data, make sure you are importing using .head or .tail
 data.tail()
 
-#check your data - rounded to the second decimal
-round(data.describe(), 2)
+# Create an Incrase/Decrease column 
+data["Increase/Decrease"] = data['Profit/Losses'] - data['Profit/Losses'].shift()
 
-#Checking data quality - nulls - first null on %chng is natural
-data.index.dtype()
+# Creating a daily percent change column 
+data["percent_chg"] = data['Profit/Losses'].pct_change()
+
+# Check new dataframe
+data.tail()
+
+# Checking data quality - nulls - first null on %chng is natural
 data.isnull()
 data.isnull().sum()
 data.isnull().mean()*100
 
-#Checking data quality - duplicate rows
+# Checking data quality - duplicate rows
 data.duplicated()
 
-#converting Profit/Losses column into floats
+# Converting Profit/Losses, Increase/Decrease, Percent_change column into floats
 data['Profit/Losses'] = data['Profit/Losses'].astype('float')
-data['% chng'] = data['% chng'].astype('float')
-data.dtypes
+data['Increase/Decrease'] = data['Increase/Decrease'].astype('float')
+data['percent_chg'] = data['percent_chg'].astype('float')
 
-#check data consistency 
-print ("\n")
-print (round(df.describe(), 2))
+# Check your data - rounded to the second decimal
+round(data.describe(), 2)
 
 
-#creating variables max increase of percentage 
-max_percent_increase = data.max()[1]
-percent_chg_max = data.loc[data['% chg'] == max_percent_increase]
+# Creating variables max increase of Increase/Decrease 
+max_increase = data.max()[1]
+max_inc_dec = data.loc[data['Increase/Decrease'] == max_increase]
 
-#creating variable max decrease of percentage
-max_percent_decrease = data.min()[1]
-percent_chg_min = data.loc[data['% chg'] == max_percent_decrease]
+# Creating variables max increase of Increase/Decrease 
+min_increase = data.min()[1]
+min_inc_dec = data.loc[data['Increase/Decrease'] == min_increase]
 
-#creating values for requested variables
-total_months = df.count()
-total_pnl = df.sum()
-max_increase = df.max()
+# Creating variable average change of Increase/Decrease 
+average_mean = data.mean()[1]
 
-#mean_change = data.mean()
-#max_increase = data.max()
-#min_decrease = data.min()
+# Creating values for requested variables
+total_months = data.count()
+total_pnl = data.sum()
 
 print ("\n")
-print ("Financial Analysis: \n --------------------")
+print ("Financial Analysis: \n--------------------")
 print (f"Total Months: {total_months[0]}")
 print (f"Total PnL: {total_pnl [0]}")
-#print (f"Average Change: {mean_change [0]}")
-#print (f"Greatest Increase in Profits: {max_increase [0]}")
-#print (f"Greatest Decrease in Profits: {min_decrease [0]}")
-
-
+print (f"Average Change: {average_mean}")
+print (f"Greatest Increase in Profits: {max_inc_dec.index[0]} {max_inc_dec.iloc[0,1]}")
+print (f"Greatest Decrease in Profits: {min_inc_dec.index[0]} {min_inc_dec.iloc[0,1]}")
